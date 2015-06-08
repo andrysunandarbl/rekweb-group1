9802c1f8ab587166c9f188734e6159e89083c0b0
andry <kitapiknik73@gmail.com>
2015-06-09 06:48:10 +0700

    Update configuration and add first source

---
+++ src/main/java/com/bl/rekweb/group1/AppMain.java
@@ -0,0 +1,23 @@
+/**
+ * IAM-Soft, Proprietary Software Cloud Communications
+ *  Copyright (c) 2015, IAM-Software and individual contributors
+ *  by the @authors tag.
+ *
+ *  This program is Proprietary Software: you can not redistribute it and/or modify
+ *  without license from IAM-Software.
+ *
+ *  Website : http://www.iam-software.com/
+ *  Report bugs to <techsupport@iam-software.com>.
+ *  Copyright (C) 2015 PT. IAM-Software. All rights reserved.
+ */
+package com.bl.rekweb.group1;
+
+/**
+ * Author andry on 09/06/15.
+ */
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.println("This is group 1");
+    }
+}
